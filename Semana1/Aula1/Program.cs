d6a7d1c6658bc7204ddc839b89d361720f696afa
mfnswdev <engenharia.mfn@gmail.com>
2023-11-08 12:37:40 -0300

    Exemplos e MeuPrimeiroPrograma

--- Semana1/Aula1/Program.cs
+++ Semana1/Aula1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region tipos de dados
+
+int tipoInteiro;
+double tipoDouble;
+bool tipoBooleano;
+string tipoTexto = "100";
+long tipoInteiroLong;
+
+tipoInteiro = int.MaxValue;
+tipoDouble = double.MaxValue;
+tipoInteiroLong = long.MaxValue;
+
+tipoInteiroLong = 10;
+
+tipoInteiro = int.Parse(tipoTexto);
+
+System.Console.WriteLine(tipoInteiro);
+Console.WriteLine(tipoDouble);
+System.Console.WriteLine(tipoInteiroLong);
+System.Console.WriteLine(tipoTexto);
+
+#endregion
