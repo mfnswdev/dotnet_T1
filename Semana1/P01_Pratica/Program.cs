1cc82405866b4889c3dce01ec0f890d5ab216666
mfnswdev <engenharia.mfn@gmail.com>
2023-11-09 21:48:32 -0300

    Ajuste e refatoracao de codigos P01

--- Semana1/P01_Pratica/Program.cs
+++ Semana1/P01_Pratica/Program.cs
@@ -1,33 +1,18 @@
 ﻿#region Dados numéricos tipo int no C#(.net)
-        
         int exemploInt = 42;
         Console.WriteLine($"Exemplo int: {exemploInt}");
-
-        // 2. long: Representa números inteiros de 64 bits
         long exemploLong = 1234567890123456;
         Console.WriteLine($"Exemplo long: {exemploLong}");
-
-        // 3. short: Representa números inteiros de 16 bits
         short exemploShort = 32767;
         Console.WriteLine($"Exemplo short: {exemploShort}");
-
-        // 4. byte: Representa números inteiros de 8 bits
         byte exemploByte = 255;
         Console.WriteLine($"Exemplo byte: {exemploByte}");
-
-        // 5. sbyte: Representa números inteiros de 8 bits com sinal
         sbyte exemploSByte = -128;
         Console.WriteLine($"Exemplo sbyte: {exemploSByte}");
-
-        // 6. ushort: Representa números inteiros sem sinal de 16 bits
         ushort exemploUShort = 65535;
         Console.WriteLine($"Exemplo ushort: {exemploUShort}");
-
-        // 7. uint: Representa números inteiros sem sinal de 32 bits
         uint exemploUInt = 4294967295;
         Console.WriteLine($"Exemplo uint: {exemploUInt}");
-
-        // 8. ulong: Representa números inteiros sem sinal de 64 bits
         ulong exemploULong = 18446744073709551615;
         Console.WriteLine($"Exemplo ulong: {exemploULong}");
 #endregion
