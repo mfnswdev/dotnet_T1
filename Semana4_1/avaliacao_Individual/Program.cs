0e117fb7127f7efee9afff54c79b6361c37dc043
mfnswdev <engenharia.mfn@gmail.com>
2023-11-29 10:45:16 -0300

    add Classes Medico and Paciente

--- Semana4_1/avaliacao_Individual/Program.cs
+++ Semana4_1/avaliacao_Individual/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+#region Classes
+public class Medico
+{
+    public string Nome { get; set; }
+    public string DataNascimento { get; set; }
+    public double Cpf { get; set; }
+    public int Crm { get; set; }
+}
+
+public class Paciente
+{
+    public string Nome { get; set; }
+    public string DataNascimento { get; set; }
+    public double Cpf { get; set; }
+    public string Sexo { get; set; }
+    public string Sintomas { get; set; }
+}
+
+#endregion
+
+#region Requisitos
+
+
+
+#endregion
+
+
+#region Relatórios
+
+
+#endregion
