52e9736480e9ca866715e87ee79af350c316d201
mfnswdev <engenharia.mfn@gmail.com>
2023-11-29 10:51:43 -0300

    fix commit Colections

--- Semana4_1/avaliacao_Individual/Program.cs
+++ Semana4_1/avaliacao_Individual/Program.cs
@@ -23,6 +23,55 @@ public class Paciente
 
 #region Requisitos
 
+public class Program
+{
+    public static void Main()
+    {
+        List<Paciente> pacientes = new List<Paciente>();
+
+        Paciente paciente1 = new Paciente()
+        {
+            Nome = "John Doe",
+            DataNascimento = "01/01/1980",
+            Cpf = 12345678901,
+        };
+
+        // Check if the new paciente's CPF 
+        if (pacientes.Exists(m => m.Cpf == paciente1.Cpf))
+        {
+            Console.WriteLine("CPF number already exists in the collection.");
+            // Handle the case when a duplicate CPF is found
+        }
+        else
+        {
+            pacientes.Add(paciente1);
+            // Continue adding the medico to the collection
+        }
+        List<Medico> medicos = new List<Medico>();
+
+        Medico medico1 = new Medico()
+        {
+            Nome = "John Doe",
+            DataNascimento = "01/01/1980",
+            Cpf = 12345678901,
+            Crm = 12345
+        };
+
+        // Check if the new medico's CPF or CRM already exist in the collection
+        if (medicos.Exists(m => m.Cpf == medico1.Cpf || m.Crm == medico1.Crm))
+        {
+            Console.WriteLine("CPF or CRM number already exists in the collection.");
+            // Handle the case when a duplicate CPF or CRM is found
+        }
+        else
+        {
+            medicos.Add(medico1);
+            // Continue adding the medico to the collection
+        }
+
+    }
+}
+
 
 
 #endregion
