4c0b4ca5a3dade48e47fdacea5a8e44b7c9fe0d0
mfnswdev <engenharia.mfn@gmail.com>
2024-02-22 16:30:28 -0300

    Minimal MVC example with Encryptation/Security

---
+++ mvc_Exercise/MvcMovie/Models/User.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class User
+{
+   public int Id { get; set; }
+   public string Name { get; set; } = null!;
+   public string Email { get; set; } = null!;
+   public string Password { get; set; } = null!; 
+}
