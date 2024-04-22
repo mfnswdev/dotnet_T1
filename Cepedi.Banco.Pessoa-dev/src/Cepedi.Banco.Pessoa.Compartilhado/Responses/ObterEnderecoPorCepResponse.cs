b07615bf03aa50dfb611e982bd894af7e3a8442f
mfnswdev <engenharia.mfn@gmail.com>
2024-04-22 15:51:47 -0300

    add Pratica Consumo de API de CEP

---
+++ Cepedi.Banco.Pessoa-dev/src/Cepedi.Banco.Pessoa.Compartilhado/Responses/ObterEnderecoPorCepResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Cepedi.Banco.Pessoa.Compartilhado.Responses;
+
+
+public class ObterEnderecoPorCepResponse
+{
+
+    public string cep { get; set; } = default!;
+    public string logradouro { get; set; } = default!;
+    public string complemento { get; set; } = default!;
+    public string bairro { get; set; } = default!;
+    public string localidade { get; set; } = default!;
+    public string uf { get; set; } = default!;
+    public string ibge { get; set; } = default!;
+    public string gia { get; set; } = default!;
+    public string ddd { get; set; } = default!;
+    public string siafi { get; set; } = default!;
+}
---
+++ Cepedi.Banco.Pessoa-dev/src/Cepedi.Banco.Pessoa.Dominio/Handlers/Endereco/ObterEnderecoPorCepRequestHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Text.Json;
+using Cepedi.Banco.Pessoa.Compartilhado.Requests;
+using Cepedi.Banco.Pessoa.Compartilhado.Responses;
+using Cepedi.Banco.Pessoa.Dominio.Entidades;
+using Cepedi.Banco.Pessoa.Dominio.Repository;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using OperationResult;
+
+namespace Cepedi.Banco.Pessoa.Dominio.Handlers;
+
+public class ObterEnderecoPorCepRequestHandler : IRequestHandler<ObterEnderecoPorCepRequest, Result<ObterEnderecoPorCepResponse>>
+{
+    private readonly IEnderecoRepository _enderecoRepository;
+    private readonly ILogger<ObterEnderecoPorCepRequestHandler> _logger;
+    public ObterEnderecoPorCepRequestHandler(IEnderecoRepository enderecoRepository, ILogger<ObterEnderecoPorCepRequestHandler> logger)
+    {
+        _enderecoRepository = enderecoRepository;
+        _logger = logger;
+    }
+    public async Task<Result<ObterEnderecoPorCepResponse>> Handle(ObterEnderecoPorCepRequest request, CancellationToken cancellationToken)
+    {
+        var endereco = await GetEndereco(request.Cep);
+        
+        if (endereco == null)
+        {
+            return Result.Error<ObterEnderecoPorCepResponse>(new Compartilhado.Exceptions.SemResultadosExcecao());
+        }
+
+        return Result.Success(endereco);
+    }
+
+    public async Task<ObterEnderecoPorCepResponse> GetEndereco(string cep)
+    {
+        string url = $"https://viacep.com.br/ws/{cep}/json/";
+
+        var client = new HttpClient();
+        var response = await client.GetAsync(url);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                var responseContentError = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("cai aqui");
+                return JsonSerializer.Deserialize<ObterEnderecoPorCepResponse>(responseContentError);
+            }
+            else
+                throw new Exception($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+        }
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var endereco = JsonSerializer.Deserialize<ObterEnderecoPorCepResponse>(responseContent);
+        return endereco;
+    }
+}
