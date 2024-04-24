fde2aefdb106f06b4f232578f2366d3715ca75ee
mfnswdev <engenharia.mfn@gmail.com>
2024-04-24 17:09:19 -0300

    add Exercicio implementação RabbitMQ fila mensagem

---
+++ RabbitMQ_exercicio/RabbitProducer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Program{
+    public static void Main(string[] args)
+    {
+        sendMessage();
+    }
+
+        static void sendMessage()
+        {
+
+            var factory = new ConnectionFactory() { HostName = "srv508250.hstgr.cloud", UserName = "aluno", Password = "changeme", Port = 5672 };
+
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello-mfn",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Alô, MFN!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello-mfn",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            };
+
+            
+        }
+    }
+    
+
+
